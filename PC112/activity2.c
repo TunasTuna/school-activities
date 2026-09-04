8ee16e6828d4c9cd8e636be8ef940e95fc1fed45
ShoHuu <83387636+TunasTuna@users.noreply.github.com>
2026-09-04 09:18:00 +0800

    Update activity2.c

--- PC112/activity2.c
+++ PC112/activity2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main () {
+    printf("  ********\n");
+    printf(" ********** \n");
+    printf("************\n");
+    printf("*   ****   *\n");
+    printf("************\n");
+    printf("*****  *****\n");
+    printf(" **********\n");
+    printf("  ** ** **\n");
+    printf("  ** ** **\n");
 
+    printf("I chose this skull because since I was a kid, I’ve always pirated software behind paywalls (like Minecraft, Terraria, etc.) even when I had no money.  I’ll stay clever in life by turning the limitations into opportunities just like I did with software since childhood. I’ll keep using that same ability to get what I want in life.");
+    return 0;
+}
