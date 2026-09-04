25189dc25bb2f7375cecb6231f55f83e217a5b54
ShoHuu <83387636+TunasTuna@users.noreply.github.com>
2026-09-04 09:14:22 +0800

    Add gotoxy function and print formatted names

--- PC112/activity3.c
+++ PC112/activity3.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+void gotoxy(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+
+}
+
+int main () {
+
+    gotoxy(1, 5);
+    printf("first_name");
+    
+    gotoxy(125, 5);
+    printf("surname");
+
+    gotoxy(63, 10);
+    printf("middle_name");
+
+    gotoxy(1, 30);
+    printf("first_name");
+
+    gotoxy(125, 30);
+    printf("surname");
+
+    return 0;
+}
